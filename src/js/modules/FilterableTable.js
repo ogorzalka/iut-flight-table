d9371362b3ddcaa9ed5db265823ce8b59a640f81
Olivier Gorzalka <olivier@amphibee.fr>
2024-12-05 13:21:42 +0100

    📝 (index.html): add comment to explain the purpose of the page-changed event ♻️ (FilterableTable.js): refactor function parameter name from jsonDatas to jsonFile for clarity 💡 (FilterableTable.js): add comments to describe the purpose of methods in FilterableTable ♻️ (FilterableTable.js): update totalItems method to return the length of datas instead of items ♻️ (Pagination.js): refactor currentPage initialization to default to 1 and add init method for clarity 💡 (Pagination.js): add comments to describe the purpose of methods in Pagination

--- src/js/modules/FilterableTable.js
+++ src/js/modules/FilterableTable.js
@@ -1,4 +1,4 @@
-export default function FilterableTable(jsonDatas) {
+export default function FilterableTable(jsonFile) {
     return {
         datas: [],
         items: [],
@@ -6,8 +6,22 @@ export default function FilterableTable(jsonDatas) {
         currentPage: 1,
         loading: true,
         init() {
+            this.paginationInit();
+            this.fetchDatas(jsonFile);
+        },
+
+        /*
+         * Initialise les paramètres de pagination
+         */
+        paginationInit() {
             this.perPage = parseInt(this.$el.dataset.perPage || 10);
-            fetch(jsonDatas)
+        },
+
+        /*
+         * Initialise les données avec le JSON fourni
+         */
+        fetchDatas(file) {
+            fetch(file)
                 .then(response => response.json())
                 .then(data => {
                     this.setDatas(data);
@@ -18,17 +32,33 @@ export default function FilterableTable(jsonDatas) {
                     this.loading = false;
                 });
         },
+
+        /*
+         * Initialiser les tableaux de données
+         */
         setDatas(data) {
             this.datas = data;
             this.items = this.paginateData();
         },
+
+        /*
+         * Retourne le tableau d'élément filtré selon la page courante
+         */
         paginateData() {
             const start = (this.currentPage - 1) * this.perPage;
             return this.datas.slice(start, start + this.perPage);
         },
+
+        /*
+         * Retourne le nombre total d'items dans la liste
+         */
         totalItems() {
-            return this.items.length;
+            return this.datas.length;
         },
+
+        /*
+         * Met à jour pour définir la page courante et mettre à jour les éléments affichés
+         */
         updateCurrentPage(event) {
             this.currentPage = event.detail;
             this.items = this.paginateData();
--- src/js/modules/Pagination.js
+++ src/js/modules/Pagination.js
@@ -1,12 +1,23 @@
 export default function Pagination(totalItems, currentPage, perPage) {
     return {
         totalItems,
-        currentPage,
+        currentPage: 1,
         perPage,
+        init() {
+            this.currentPage = currentPage;
+        },
+        /*
+         * Récupère le nombre de pages total
+         */
         totalPages() {
             return Math.ceil(this.totalItems / this.perPage);
         },
+        /*
+         * Permet d'accéder à redéfinir le numéro de page courante
+         * et transmet un évènement au composant parent
+         */
         goToPage(page) {
+            this.currentPage = page;
             this.$dispatch('page-changed', page);
         },
     }
